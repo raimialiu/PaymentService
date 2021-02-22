f000d51e481a167b910fe0df777b10af326c5444
Raimi.Aliu <Raimi.Aliu@fcmb.com>
2021-02-22 05:23:33 +0100

    created base service for making payment

---
+++ ProcessPayment/DTOs/PaymentDTOs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessPayment.DTOs
+{
+    public class PaymentDTOs
+    {
+        [Required]
+        public string CardHolderName { get; set; }
+        [Required]
+        public string CreditCardNumber { get; set; }
+        [Required]
+        public DateTime ExpirationDate { get; set; }
+        [Required]
+        public string SecurityCode { get; set; }
+        [Required]
+        
+        public decimal Amount { get; set; }
+    }
+}
--- ProcessPayment/Services/ICheapPaymentGateway.cs
+++ ProcessPayment/Services/ICheapPaymentGateway.cs
@@ -1,16 +1,53 @@
-﻿using System;
+﻿using ProcessPayment.DTOs;
+using ProcessPayment.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProcessPayment.Services
 {
-    public interface ICheapPaymentGateway
+    public interface ICheapPaymentGateway:IProcessPayment
     {
     }
 
-    public interface ProcessPayment
+    public interface IExpensivePaymentGateway:IProcessPayment
     {
-        public bool MakePayment();
+
+    }
+
+    public interface IProcessPayment
+    {
+        public bool MakePayment(PaymentDTOs dto);
+    }
+
+
+    public class ProcessPayment : IProcessPayment
+    {
+        private ProcessPaymentDbContext _ctx;
+        private IProcessPayment _payment;
+
+        private ProcessPayment instance;
+
+        private object lockArea = new object();
+        public ProcessPayment createInstance(IProcessPayment payment)
+        {
+            lock(lockArea)
+            {
+                if (instance == null)
+                    return new ProcessPayment(payment);
+            }
+            
+            return instance;
+        }
+        private ProcessPayment(IProcessPayment pay)
+        {
+            _ctx = new ProcessPaymentDbContext();
+            this._payment = pay;
+        }
+        public bool MakePayment(PaymentDTOs dto)
+        {
+            return _payment.MakePayment(dto);
+        }
     }
 }
