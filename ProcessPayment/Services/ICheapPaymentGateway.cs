1b043c0733c9e7a2a799394018f7ee77365a87b5
Raimi.Aliu <Raimi.Aliu@fcmb.com>
2021-02-22 05:12:46 +0100

    added services for different payment

---
+++ ProcessPayment/Services/ICheapPaymentGateway.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProcessPayment.Services
+{
+    public interface ICheapPaymentGateway
+    {
+    }
+
+    public interface ProcessPayment
+    {
+        public bool MakePayment();
+    }
+}
